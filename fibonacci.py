6356dbc50b498f029dd93ebf139387279e244b16
ychsu1004 <yanchao.hsu@gmail.com>
2022-02-18 20:51:25 +0800

    fibonacci number

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+def calc_fib(n):
+    if n < 2:
+        return n
+    else:
+        return calc_fib(n-1) + calc_fib(n-2)
+
+fib = int(input("input a number:"))
+for i in range(fib):
+    print( calc_fib(i), end=' ')
